02ec9ed9a060f589660e88c74c4aad010cdecce0
Admin <qwerty901cx@gmail.com>
2024-07-20 23:05:26 +0300

    Поместил в папку out (без неё не запускается программа) и выполнил задание
    Не получилось сделать через boolean (в разборе дз булиня не было).

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        number();
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+    }
+
+    public static int inputNumber() {
+        System.out.println("Введи число: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void number() {
+        System.out.println(inputNumber() % 2 == 0);
     }
 }
